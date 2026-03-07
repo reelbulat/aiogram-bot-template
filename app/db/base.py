cde85c3cc5f7500d3b64bfb63c51b87faaa01dce
reelbulat <leavingpost@gmail.com>
2026-03-07 23:31:09 +0300

    Create base.py

---
+++ app/db/base.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from app.config import DATABASE_URL
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    future=True,
+)
+
+Base = declarative_base()
